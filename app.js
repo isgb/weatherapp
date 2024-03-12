012f1dd61bc3ae49144efa3b7262ee7c1effcd1b
oscar <oscar.vasquez@zentro.com.mx>
2024-03-12 16:02:51 -0600

    debug

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ import chalk from "chalk";
 
 const API_KEY = "2ba1a6045ce8be8a75cc7dad72102e1f";
 
+async function getWeather(city){
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 function getData() {
     let city = process.argv[2];
     // console.log(city);
@@ -11,6 +19,8 @@ function getData() {
         console.log(chalk.red("Por favor, proporcionar un nombre de lugar o ciudad"));
         console.log(chalk.red("Ejecuta el comando de la siguiente forma: node app.js [nombre ciudad]"));
     }
+
+    getWeather(city).then().catch();
 }
 
 getData();
