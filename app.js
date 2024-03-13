ca7160fdc9ec3344494540ab4be18b7d5688cf60
oscar <oscar.vasquez@zentro.com.mx>
2024-03-13 15:24:45 -0600

    debug

--- app.js
+++ app.js
@@ -3,12 +3,36 @@ import chalk from "chalk";
 
 const API_KEY = "2ba1a6045ce8be8a75cc7dad72102e1f";
 
-async function getWeather(city){
+function displayWeather(city, weatherData) {
+    console.log(chalk.yellow(`\nInformación del clima: ${city}:`));
+    console.log(
+      chalk.yellow(
+        "☀️🌙❄️🌡️💧🌈🌪️🌧️☀️🌙❄️🌡️💧🌈🌪️🌧️☀️🌙❄️🌡️💧🌈🌪️🌧️☀️🌙❄️🌡️💧🌈🌪️🌧️"
+      )
+    );
+    console.log(chalk.cyan("Descripción:"), weatherData.weather[0].description);
+    console.log(chalk.cyan("Temperatura:"), `${weatherData.main.temp} °C`);
+    console.log(chalk.cyan("Humedad:"), `${weatherData.main.humidity}%`);
+    console.log(
+      chalk.cyan("Velocidad del Viento:"),
+      `${weatherData.wind.speed} m/s`
+    );
+    console.log(
+      chalk.yellow("☀️🌙❄️🌡️💧🌈🌪️🌧️☀️🌙❄️🌡️💧🌈🌪️🌧️☀️🌙❄️🌡️💧🌈🌪️🌧️\n")
+    );
+  }
+
+function handleError(err) {
+    console.log(chalk.red("Error: "), err.message)
+    process.exit(1)
+}
+
+async function getWeather(city) {
     try {
         let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
 
         const response = await axios.get(endpoint, {
-            params:{
+            params: {
                 q: city,
                 appid: API_KEY,
                 units: "metric"
@@ -26,19 +50,22 @@ async function getWeather(city){
     }
 }
 
-function getData() {
+function initApp() {
     let city = process.argv[2];
     // console.log(city);
 
-    if(!city){
+    if (!city) {
         console.log(chalk.red("Por favor, proporcionar un nombre de lugar o ciudad"));
         console.log(chalk.red("Ejecuta el comando de la siguiente forma: node app.js [nombre ciudad]"));
     }
 
-    getWeather(city);
+    getWeather(city)
+    .then(weatherData => displayWeather(city, weatherData))
+    .catch(handleError);
 }
 
-getData();
+initApp()
+    
 
 // https://www.youtube.com/watch?v=0f26_Enlv38&t=24440s
 // 7:43:02
